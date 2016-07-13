javiergamarra/liferay-portal

LPS-67034 autogen services

modules/apps/screens/screens-api/src/main/java/com/liferay/screens/service/ScreensRatingsEntryService.java
@@ -1,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.screens.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
+import com.liferay.portal.kernel.security.access.control.AccessControlled;
+import com.liferay.portal.kernel.service.BaseService;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portal.kernel.transaction.Isolation;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.Transactional;
+
+/**
+ * Provides the remote service interface for ScreensRatingsEntry. Methods of this
+ * service are expected to have security checks based on the propagated JAAS
+ * credentials because this service can be accessed remotely.
+ *
+ * @author José Manuel Navarro
+ * @see ScreensRatingsEntryServiceUtil
+ * @see com.liferay.screens.service.base.ScreensRatingsEntryServiceBaseImpl
+ * @see com.liferay.screens.service.impl.ScreensRatingsEntryServiceImpl
+ * @generated
+ */
+@AccessControlled
+@JSONWebService
+@OSGiBeanProperties(property =  {
+	"json.web.service.context.name=screens", "json.web.service.context.path=ScreensRatingsEntry"}, service = ScreensRatingsEntryService.class)
+@ProviderType
+@Transactional(isolation = Isolation.PORTAL, rollbackFor =  {
+	PortalException.class, SystemException.class})
+public interface ScreensRatingsEntryService extends BaseService {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify or reference this interface directly. Always use {@link ScreensRatingsEntryServiceUtil} to access the screens ratings entry remote service. Add custom service methods to {@link com.liferay.screens.service.impl.ScreensRatingsEntryServiceImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+	 */
+	public JSONObject deleteRatingEntry(long classPK,
+		java.lang.String className, int stepCount) throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public JSONObject getRatingsEntries(long classPK,
+		java.lang.String className, int stepCount) throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public JSONObject getRatingsEntries(long entryId, int stepCount)
+		throws PortalException;
+
+	public JSONObject updateRatingEntry(long classPK,
+		java.lang.String className, double score, int stepCount)
+		throws PortalException;
+
+	/**
+	* Returns the OSGi service identifier.
+	*
+	* @return the OSGi service identifier
+	*/
+	public java.lang.String getOSGiServiceIdentifier();
+}

modules/apps/screens/screens-api/src/main/java/com/liferay/screens/service/ScreensRatingsEntryServiceUtil.java
@@ -1,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.screens.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.osgi.util.ServiceTrackerFactory;
+
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * Provides the remote service utility for ScreensRatingsEntry. This utility wraps
+ * {@link com.liferay.screens.service.impl.ScreensRatingsEntryServiceImpl} and is the
+ * primary access point for service operations in application layer code running
+ * on a remote server. Methods of this service are expected to have security
+ * checks based on the propagated JAAS credentials because this service can be
+ * accessed remotely.
+ *
+ * @author José Manuel Navarro
+ * @see ScreensRatingsEntryService
+ * @see com.liferay.screens.service.base.ScreensRatingsEntryServiceBaseImpl
+ * @see com.liferay.screens.service.impl.ScreensRatingsEntryServiceImpl
+ * @generated
+ */
+@ProviderType
+public class ScreensRatingsEntryServiceUtil {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to {@link com.liferay.screens.service.impl.ScreensRatingsEntryServiceImpl} and rerun ServiceBuilder to regenerate this class.
+	 */
+	public static com.liferay.portal.kernel.json.JSONObject deleteRatingEntry(
+		long classPK, java.lang.String className, int stepCount)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService().deleteRatingEntry(classPK, className, stepCount);
+	}
+
+	public static com.liferay.portal.kernel.json.JSONObject getRatingsEntries(
+		long classPK, java.lang.String className, int stepCount)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService().getRatingsEntries(classPK, className, stepCount);
+	}
+
+	public static com.liferay.portal.kernel.json.JSONObject getRatingsEntries(
+		long entryId, int stepCount)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService().getRatingsEntries(entryId, stepCount);
+	}
+
+	public static com.liferay.portal.kernel.json.JSONObject updateRatingEntry(
+		long classPK, java.lang.String className, double score, int stepCount)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService()
+				   .updateRatingEntry(classPK, className, score, stepCount);
+	}
+
+	/**
+	* Returns the OSGi service identifier.
+	*
+	* @return the OSGi service identifier
+	*/
+	public static java.lang.String getOSGiServiceIdentifier() {
+		return getService().getOSGiServiceIdentifier();
+	}
+
+	public static ScreensRatingsEntryService getService() {
+		return _serviceTracker.getService();
+	}
+
+	private static ServiceTracker<ScreensRatingsEntryService, ScreensRatingsEntryService> _serviceTracker =
+		ServiceTrackerFactory.open(ScreensRatingsEntryService.class);
+}

modules/apps/screens/screens-api/src/main/java/com/liferay/screens/service/ScreensRatingsEntryServiceWrapper.java
@@ -1,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.screens.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.service.ServiceWrapper;
+
+/**
+ * Provides a wrapper for {@link ScreensRatingsEntryService}.
+ *
+ * @author José Manuel Navarro
+ * @see ScreensRatingsEntryService
+ * @generated
+ */
+@ProviderType
+public class ScreensRatingsEntryServiceWrapper
+	implements ScreensRatingsEntryService,
+		ServiceWrapper<ScreensRatingsEntryService> {
+	public ScreensRatingsEntryServiceWrapper(
+		ScreensRatingsEntryService screensRatingsEntryService) {
+		_screensRatingsEntryService = screensRatingsEntryService;
+	}
+
+	@Override
+	public com.liferay.portal.kernel.json.JSONObject deleteRatingEntry(
+		long classPK, java.lang.String className, int stepCount)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _screensRatingsEntryService.deleteRatingEntry(classPK,
+			className, stepCount);
+	}
+
+	@Override
+	public com.liferay.portal.kernel.json.JSONObject getRatingsEntries(
+		long classPK, java.lang.String className, int stepCount)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _screensRatingsEntryService.getRatingsEntries(classPK,
+			className, stepCount);
+	}
+
+	@Override
+	public com.liferay.portal.kernel.json.JSONObject getRatingsEntries(
+		long entryId, int stepCount)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _screensRatingsEntryService.getRatingsEntries(entryId, stepCount);
+	}
+
+	@Override
+	public com.liferay.portal.kernel.json.JSONObject updateRatingEntry(
+		long classPK, java.lang.String className, double score, int stepCount)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _screensRatingsEntryService.updateRatingEntry(classPK,
+			className, score, stepCount);
+	}
+
+	/**
+	* Returns the OSGi service identifier.
+	*
+	* @return the OSGi service identifier
+	*/
+	@Override
+	public java.lang.String getOSGiServiceIdentifier() {
+		return _screensRatingsEntryService.getOSGiServiceIdentifier();
+	}
+
+	@Override
+	public ScreensRatingsEntryService getWrappedService() {
+		return _screensRatingsEntryService;
+	}
+
+	@Override
+	public void setWrappedService(
+		ScreensRatingsEntryService screensRatingsEntryService) {
+		_screensRatingsEntryService = screensRatingsEntryService;
+	}
+
+	private ScreensRatingsEntryService _screensRatingsEntryService;
+}

modules/apps/screens/screens-service/src/main/java/com/liferay/screens/service/http/ScreensRatingsEntryServiceSoap.java
@@ -1,0 +1,119 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.screens.service.http;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import com.liferay.screens.service.ScreensRatingsEntryServiceUtil;
+
+import java.rmi.RemoteException;
+
+/**
+ * Provides the SOAP utility for the
+ * {@link ScreensRatingsEntryServiceUtil} service utility. The
+ * static methods of this class calls the same methods of the service utility.
+ * However, the signatures are different because it is difficult for SOAP to
+ * support certain types.
+ *
+ * <p>
+ * The benefits of using the SOAP utility is that it is cross platform
+ * compatible. SOAP allows different languages like Java, .NET, C++, PHP, and
+ * even Perl, to call the generated services. One drawback of SOAP is that it is
+ * slow because it needs to serialize all calls into a text format (XML).
+ * </p>
+ *
+ * <p>
+ * You can see a list of services at http://localhost:8080/api/axis. Set the
+ * property <b>axis.servlet.hosts.allowed</b> in portal.properties to configure
+ * security.
+ * </p>
+ *
+ * <p>
+ * The SOAP utility is only generated for remote services.
+ * </p>
+ *
+ * @author José Manuel Navarro
+ * @see ScreensRatingsEntryServiceHttp
+ * @see ScreensRatingsEntryServiceUtil
+ * @generated
+ */
+@ProviderType
+public class ScreensRatingsEntryServiceSoap {
+	public static java.lang.String deleteRatingEntry(long classPK,
+		java.lang.String className, int stepCount) throws RemoteException {
+		try {
+			com.liferay.portal.kernel.json.JSONObject returnValue = ScreensRatingsEntryServiceUtil.deleteRatingEntry(classPK,
+					className, stepCount);
+
+			return returnValue.toString();
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	public static java.lang.String updateRatingEntry(long classPK,
+		java.lang.String className, double score, int stepCount)
+		throws RemoteException {
+		try {
+			com.liferay.portal.kernel.json.JSONObject returnValue = ScreensRatingsEntryServiceUtil.updateRatingEntry(classPK,
+					className, score, stepCount);
+
+			return returnValue.toString();
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	public static java.lang.String getRatingsEntries(long entryId, int stepCount)
+		throws RemoteException {
+		try {
+			com.liferay.portal.kernel.json.JSONObject returnValue = ScreensRatingsEntryServiceUtil.getRatingsEntries(entryId,
+					stepCount);
+
+			return returnValue.toString();
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	public static java.lang.String getRatingsEntries(long classPK,
+		java.lang.String className, int stepCount) throws RemoteException {
+		try {
+			com.liferay.portal.kernel.json.JSONObject returnValue = ScreensRatingsEntryServiceUtil.getRatingsEntries(classPK,
+					className, stepCount);
+
+			return returnValue.toString();
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(ScreensRatingsEntryServiceSoap.class);
+}

salvatejero/com-liferay-screens

screens-api/src/main/java/com/liferay/screens/service/ScreensRatingsEntryService.java
@@ -1,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.screens.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
+import com.liferay.portal.kernel.security.access.control.AccessControlled;
+import com.liferay.portal.kernel.service.BaseService;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portal.kernel.transaction.Isolation;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.Transactional;
+
+/**
+ * Provides the remote service interface for ScreensRatingsEntry. Methods of this
+ * service are expected to have security checks based on the propagated JAAS
+ * credentials because this service can be accessed remotely.
+ *
+ * @author José Manuel Navarro
+ * @see ScreensRatingsEntryServiceUtil
+ * @see com.liferay.screens.service.base.ScreensRatingsEntryServiceBaseImpl
+ * @see com.liferay.screens.service.impl.ScreensRatingsEntryServiceImpl
+ * @generated
+ */
+@AccessControlled
+@JSONWebService
+@OSGiBeanProperties(property =  {
+	"json.web.service.context.name=screens", "json.web.service.context.path=ScreensRatingsEntry"}, service = ScreensRatingsEntryService.class)
+@ProviderType
+@Transactional(isolation = Isolation.PORTAL, rollbackFor =  {
+	PortalException.class, SystemException.class})
+public interface ScreensRatingsEntryService extends BaseService {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify or reference this interface directly. Always use {@link ScreensRatingsEntryServiceUtil} to access the screens ratings entry remote service. Add custom service methods to {@link com.liferay.screens.service.impl.ScreensRatingsEntryServiceImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+	 */
+	public JSONObject deleteRatingsEntry(long classPK,
+		java.lang.String className, int ratingsLength)
+		throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public JSONObject getRatingsEntries(long assetEntryId, int ratingsLength)
+		throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public JSONObject getRatingsEntries(long classPK,
+		java.lang.String className, int ratingsLength)
+		throws PortalException;
+
+	public JSONObject updateRatingsEntry(long classPK,
+		java.lang.String className, double score, int ratingsLength)
+		throws PortalException;
+
+	/**
+	* Returns the OSGi service identifier.
+	*
+	* @return the OSGi service identifier
+	*/
+	public java.lang.String getOSGiServiceIdentifier();
+}

screens-api/src/main/java/com/liferay/screens/service/ScreensRatingsEntryServiceUtil.java
@@ -1,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.screens.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.osgi.util.ServiceTrackerFactory;
+
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * Provides the remote service utility for ScreensRatingsEntry. This utility wraps
+ * {@link com.liferay.screens.service.impl.ScreensRatingsEntryServiceImpl} and is the
+ * primary access point for service operations in application layer code running
+ * on a remote server. Methods of this service are expected to have security
+ * checks based on the propagated JAAS credentials because this service can be
+ * accessed remotely.
+ *
+ * @author José Manuel Navarro
+ * @see ScreensRatingsEntryService
+ * @see com.liferay.screens.service.base.ScreensRatingsEntryServiceBaseImpl
+ * @see com.liferay.screens.service.impl.ScreensRatingsEntryServiceImpl
+ * @generated
+ */
+@ProviderType
+public class ScreensRatingsEntryServiceUtil {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to {@link com.liferay.screens.service.impl.ScreensRatingsEntryServiceImpl} and rerun ServiceBuilder to regenerate this class.
+	 */
+	public static com.liferay.portal.kernel.json.JSONObject deleteRatingsEntry(
+		long classPK, java.lang.String className, int ratingsLength)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService().deleteRatingsEntry(classPK, className, ratingsLength);
+	}
+
+	public static com.liferay.portal.kernel.json.JSONObject getRatingsEntries(
+		long assetEntryId, int ratingsLength)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService().getRatingsEntries(assetEntryId, ratingsLength);
+	}
+
+	public static com.liferay.portal.kernel.json.JSONObject getRatingsEntries(
+		long classPK, java.lang.String className, int ratingsLength)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService().getRatingsEntries(classPK, className, ratingsLength);
+	}
+
+	public static com.liferay.portal.kernel.json.JSONObject updateRatingsEntry(
+		long classPK, java.lang.String className, double score,
+		int ratingsLength)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService()
+				   .updateRatingsEntry(classPK, className, score, ratingsLength);
+	}
+
+	/**
+	* Returns the OSGi service identifier.
+	*
+	* @return the OSGi service identifier
+	*/
+	public static java.lang.String getOSGiServiceIdentifier() {
+		return getService().getOSGiServiceIdentifier();
+	}
+
+	public static ScreensRatingsEntryService getService() {
+		return _serviceTracker.getService();
+	}
+
+	private static ServiceTracker<ScreensRatingsEntryService, ScreensRatingsEntryService> _serviceTracker =
+		ServiceTrackerFactory.open(ScreensRatingsEntryService.class);
+}

screens-api/src/main/java/com/liferay/screens/service/ScreensRatingsEntryServiceWrapper.java
@@ -1,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.screens.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.service.ServiceWrapper;
+
+/**
+ * Provides a wrapper for {@link ScreensRatingsEntryService}.
+ *
+ * @author José Manuel Navarro
+ * @see ScreensRatingsEntryService
+ * @generated
+ */
+@ProviderType
+public class ScreensRatingsEntryServiceWrapper
+	implements ScreensRatingsEntryService,
+		ServiceWrapper<ScreensRatingsEntryService> {
+	public ScreensRatingsEntryServiceWrapper(
+		ScreensRatingsEntryService screensRatingsEntryService) {
+		_screensRatingsEntryService = screensRatingsEntryService;
+	}
+
+	@Override
+	public com.liferay.portal.kernel.json.JSONObject deleteRatingsEntry(
+		long classPK, java.lang.String className, int ratingsLength)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _screensRatingsEntryService.deleteRatingsEntry(classPK,
+			className, ratingsLength);
+	}
+
+	@Override
+	public com.liferay.portal.kernel.json.JSONObject getRatingsEntries(
+		long assetEntryId, int ratingsLength)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _screensRatingsEntryService.getRatingsEntries(assetEntryId,
+			ratingsLength);
+	}
+
+	@Override
+	public com.liferay.portal.kernel.json.JSONObject getRatingsEntries(
+		long classPK, java.lang.String className, int ratingsLength)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _screensRatingsEntryService.getRatingsEntries(classPK,
+			className, ratingsLength);
+	}
+
+	@Override
+	public com.liferay.portal.kernel.json.JSONObject updateRatingsEntry(
+		long classPK, java.lang.String className, double score,
+		int ratingsLength)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _screensRatingsEntryService.updateRatingsEntry(classPK,
+			className, score, ratingsLength);
+	}
+
+	/**
+	* Returns the OSGi service identifier.
+	*
+	* @return the OSGi service identifier
+	*/
+	@Override
+	public java.lang.String getOSGiServiceIdentifier() {
+		return _screensRatingsEntryService.getOSGiServiceIdentifier();
+	}
+
+	@Override
+	public ScreensRatingsEntryService getWrappedService() {
+		return _screensRatingsEntryService;
+	}
+
+	@Override
+	public void setWrappedService(
+		ScreensRatingsEntryService screensRatingsEntryService) {
+		_screensRatingsEntryService = screensRatingsEntryService;
+	}
+
+	private ScreensRatingsEntryService _screensRatingsEntryService;
+}

screens-service/src/main/java/com/liferay/screens/service/http/ScreensRatingsEntryServiceSoap.java
@@ -1,0 +1,121 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.screens.service.http;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import com.liferay.screens.service.ScreensRatingsEntryServiceUtil;
+
+import java.rmi.RemoteException;
+
+/**
+ * Provides the SOAP utility for the
+ * {@link ScreensRatingsEntryServiceUtil} service utility. The
+ * static methods of this class calls the same methods of the service utility.
+ * However, the signatures are different because it is difficult for SOAP to
+ * support certain types.
+ *
+ * <p>
+ * The benefits of using the SOAP utility is that it is cross platform
+ * compatible. SOAP allows different languages like Java, .NET, C++, PHP, and
+ * even Perl, to call the generated services. One drawback of SOAP is that it is
+ * slow because it needs to serialize all calls into a text format (XML).
+ * </p>
+ *
+ * <p>
+ * You can see a list of services at http://localhost:8080/api/axis. Set the
+ * property <b>axis.servlet.hosts.allowed</b> in portal.properties to configure
+ * security.
+ * </p>
+ *
+ * <p>
+ * The SOAP utility is only generated for remote services.
+ * </p>
+ *
+ * @author José Manuel Navarro
+ * @see ScreensRatingsEntryServiceHttp
+ * @see ScreensRatingsEntryServiceUtil
+ * @generated
+ */
+@ProviderType
+public class ScreensRatingsEntryServiceSoap {
+	public static java.lang.String deleteRatingsEntry(long classPK,
+		java.lang.String className, int ratingsLength)
+		throws RemoteException {
+		try {
+			com.liferay.portal.kernel.json.JSONObject returnValue = ScreensRatingsEntryServiceUtil.deleteRatingsEntry(classPK,
+					className, ratingsLength);
+
+			return returnValue.toString();
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	public static java.lang.String getRatingsEntries(long assetEntryId,
+		int ratingsLength) throws RemoteException {
+		try {
+			com.liferay.portal.kernel.json.JSONObject returnValue = ScreensRatingsEntryServiceUtil.getRatingsEntries(assetEntryId,
+					ratingsLength);
+
+			return returnValue.toString();
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	public static java.lang.String getRatingsEntries(long classPK,
+		java.lang.String className, int ratingsLength)
+		throws RemoteException {
+		try {
+			com.liferay.portal.kernel.json.JSONObject returnValue = ScreensRatingsEntryServiceUtil.getRatingsEntries(classPK,
+					className, ratingsLength);
+
+			return returnValue.toString();
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	public static java.lang.String updateRatingsEntry(long classPK,
+		java.lang.String className, double score, int ratingsLength)
+		throws RemoteException {
+		try {
+			com.liferay.portal.kernel.json.JSONObject returnValue = ScreensRatingsEntryServiceUtil.updateRatingsEntry(classPK,
+					className, score, ratingsLength);
+
+			return returnValue.toString();
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(ScreensRatingsEntryServiceSoap.class);
+}