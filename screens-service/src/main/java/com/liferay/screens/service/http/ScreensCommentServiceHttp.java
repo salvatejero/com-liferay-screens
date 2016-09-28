brianchandotcom/liferay-portal

LPS-67386 autogen

modules/apps/screens/screens-service/src/main/java/com/liferay/screens/service/http/ScreensCommentServiceHttp.java
@@ -1,0 +1,201 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.screens.service.http;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.auth.HttpPrincipal;
+import com.liferay.portal.kernel.service.http.TunnelUtil;
+import com.liferay.portal.kernel.util.MethodHandler;
+import com.liferay.portal.kernel.util.MethodKey;
+
+import com.liferay.screens.service.ScreensCommentServiceUtil;
+
+/**
+ * Provides the HTTP utility for the
+ * {@link ScreensCommentServiceUtil} service utility. The
+ * static methods of this class calls the same methods of the service utility.
+ * However, the signatures are different because it requires an additional
+ * {@link HttpPrincipal} parameter.
+ *
+ * <p>
+ * The benefits of using the HTTP utility is that it is fast and allows for
+ * tunneling without the cost of serializing to text. The drawback is that it
+ * only works with Java.
+ * </p>
+ *
+ * <p>
+ * Set the property <b>tunnel.servlet.hosts.allowed</b> in portal.properties to
+ * configure security.
+ * </p>
+ *
+ * <p>
+ * The HTTP utility is only generated for remote services.
+ * </p>
+ *
+ * @author José Manuel Navarro
+ * @see ScreensCommentServiceSoap
+ * @see HttpPrincipal
+ * @see ScreensCommentServiceUtil
+ * @generated
+ */
+@ProviderType
+public class ScreensCommentServiceHttp {
+	public static com.liferay.portal.kernel.json.JSONObject addComment(
+		HttpPrincipal httpPrincipal, java.lang.String className, long classPK,
+		java.lang.String body)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		try {
+			MethodKey methodKey = new MethodKey(ScreensCommentServiceUtil.class,
+					"addComment", _addCommentParameterTypes0);
+
+			MethodHandler methodHandler = new MethodHandler(methodKey,
+					className, classPK, body);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception e) {
+				if (e instanceof com.liferay.portal.kernel.exception.PortalException) {
+					throw (com.liferay.portal.kernel.exception.PortalException)e;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(e);
+			}
+
+			return (com.liferay.portal.kernel.json.JSONObject)returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException se) {
+			_log.error(se, se);
+
+			throw se;
+		}
+	}
+
+	public static com.liferay.portal.kernel.json.JSONArray getComments(
+		HttpPrincipal httpPrincipal, java.lang.String className, long classPK,
+		int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		try {
+			MethodKey methodKey = new MethodKey(ScreensCommentServiceUtil.class,
+					"getComments", _getCommentsParameterTypes1);
+
+			MethodHandler methodHandler = new MethodHandler(methodKey,
+					className, classPK, start, end);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception e) {
+				if (e instanceof com.liferay.portal.kernel.exception.PortalException) {
+					throw (com.liferay.portal.kernel.exception.PortalException)e;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(e);
+			}
+
+			return (com.liferay.portal.kernel.json.JSONArray)returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException se) {
+			_log.error(se, se);
+
+			throw se;
+		}
+	}
+
+	public static int getCommentsCount(HttpPrincipal httpPrincipal,
+		java.lang.String className, long classPK)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		try {
+			MethodKey methodKey = new MethodKey(ScreensCommentServiceUtil.class,
+					"getCommentsCount", _getCommentsCountParameterTypes2);
+
+			MethodHandler methodHandler = new MethodHandler(methodKey,
+					className, classPK);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception e) {
+				if (e instanceof com.liferay.portal.kernel.exception.PortalException) {
+					throw (com.liferay.portal.kernel.exception.PortalException)e;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(e);
+			}
+
+			return ((Integer)returnObj).intValue();
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException se) {
+			_log.error(se, se);
+
+			throw se;
+		}
+	}
+
+	public static com.liferay.portal.kernel.json.JSONObject updateComment(
+		HttpPrincipal httpPrincipal, long commentId, java.lang.String body)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		try {
+			MethodKey methodKey = new MethodKey(ScreensCommentServiceUtil.class,
+					"updateComment", _updateCommentParameterTypes3);
+
+			MethodHandler methodHandler = new MethodHandler(methodKey,
+					commentId, body);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception e) {
+				if (e instanceof com.liferay.portal.kernel.exception.PortalException) {
+					throw (com.liferay.portal.kernel.exception.PortalException)e;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(e);
+			}
+
+			return (com.liferay.portal.kernel.json.JSONObject)returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException se) {
+			_log.error(se, se);
+
+			throw se;
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(ScreensCommentServiceHttp.class);
+	private static final Class<?>[] _addCommentParameterTypes0 = new Class[] {
+			java.lang.String.class, long.class, java.lang.String.class
+		};
+	private static final Class<?>[] _getCommentsParameterTypes1 = new Class[] {
+			java.lang.String.class, long.class, int.class, int.class
+		};
+	private static final Class<?>[] _getCommentsCountParameterTypes2 = new Class[] {
+			java.lang.String.class, long.class
+		};
+	private static final Class<?>[] _updateCommentParameterTypes3 = new Class[] {
+			long.class, java.lang.String.class
+		};
+}

salvatejero/com-liferay-screens

screens-service/src/main/java/com/liferay/screens/service/http/ScreensCommentServiceHttp.java
@@ -1,0 +1,235 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.screens.service.http;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.auth.HttpPrincipal;
+import com.liferay.portal.kernel.service.http.TunnelUtil;
+import com.liferay.portal.kernel.util.MethodHandler;
+import com.liferay.portal.kernel.util.MethodKey;
+
+import com.liferay.screens.service.ScreensCommentServiceUtil;
+
+/**
+ * Provides the HTTP utility for the
+ * {@link ScreensCommentServiceUtil} service utility. The
+ * static methods of this class calls the same methods of the service utility.
+ * However, the signatures are different because it requires an additional
+ * {@link HttpPrincipal} parameter.
+ *
+ * <p>
+ * The benefits of using the HTTP utility is that it is fast and allows for
+ * tunneling without the cost of serializing to text. The drawback is that it
+ * only works with Java.
+ * </p>
+ *
+ * <p>
+ * Set the property <b>tunnel.servlet.hosts.allowed</b> in portal.properties to
+ * configure security.
+ * </p>
+ *
+ * <p>
+ * The HTTP utility is only generated for remote services.
+ * </p>
+ *
+ * @author José Manuel Navarro
+ * @see ScreensCommentServiceSoap
+ * @see HttpPrincipal
+ * @see ScreensCommentServiceUtil
+ * @generated
+ */
+@ProviderType
+public class ScreensCommentServiceHttp {
+	public static com.liferay.portal.kernel.json.JSONObject addComment(
+		HttpPrincipal httpPrincipal, java.lang.String className, long classPK,
+		java.lang.String body)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		try {
+			MethodKey methodKey = new MethodKey(ScreensCommentServiceUtil.class,
+					"addComment", _addCommentParameterTypes0);
+
+			MethodHandler methodHandler = new MethodHandler(methodKey,
+					className, classPK, body);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception e) {
+				if (e instanceof com.liferay.portal.kernel.exception.PortalException) {
+					throw (com.liferay.portal.kernel.exception.PortalException)e;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(e);
+			}
+
+			return (com.liferay.portal.kernel.json.JSONObject)returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException se) {
+			_log.error(se, se);
+
+			throw se;
+		}
+	}
+
+	public static com.liferay.portal.kernel.json.JSONObject getComment(
+		HttpPrincipal httpPrincipal, long commentId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		try {
+			MethodKey methodKey = new MethodKey(ScreensCommentServiceUtil.class,
+					"getComment", _getCommentParameterTypes1);
+
+			MethodHandler methodHandler = new MethodHandler(methodKey, commentId);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception e) {
+				if (e instanceof com.liferay.portal.kernel.exception.PortalException) {
+					throw (com.liferay.portal.kernel.exception.PortalException)e;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(e);
+			}
+
+			return (com.liferay.portal.kernel.json.JSONObject)returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException se) {
+			_log.error(se, se);
+
+			throw se;
+		}
+	}
+
+	public static com.liferay.portal.kernel.json.JSONArray getComments(
+		HttpPrincipal httpPrincipal, java.lang.String className, long classPK,
+		int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		try {
+			MethodKey methodKey = new MethodKey(ScreensCommentServiceUtil.class,
+					"getComments", _getCommentsParameterTypes2);
+
+			MethodHandler methodHandler = new MethodHandler(methodKey,
+					className, classPK, start, end);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception e) {
+				if (e instanceof com.liferay.portal.kernel.exception.PortalException) {
+					throw (com.liferay.portal.kernel.exception.PortalException)e;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(e);
+			}
+
+			return (com.liferay.portal.kernel.json.JSONArray)returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException se) {
+			_log.error(se, se);
+
+			throw se;
+		}
+	}
+
+	public static int getCommentsCount(HttpPrincipal httpPrincipal,
+		java.lang.String className, long classPK)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		try {
+			MethodKey methodKey = new MethodKey(ScreensCommentServiceUtil.class,
+					"getCommentsCount", _getCommentsCountParameterTypes3);
+
+			MethodHandler methodHandler = new MethodHandler(methodKey,
+					className, classPK);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception e) {
+				if (e instanceof com.liferay.portal.kernel.exception.PortalException) {
+					throw (com.liferay.portal.kernel.exception.PortalException)e;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(e);
+			}
+
+			return ((Integer)returnObj).intValue();
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException se) {
+			_log.error(se, se);
+
+			throw se;
+		}
+	}
+
+	public static com.liferay.portal.kernel.json.JSONObject updateComment(
+		HttpPrincipal httpPrincipal, long commentId, java.lang.String body)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		try {
+			MethodKey methodKey = new MethodKey(ScreensCommentServiceUtil.class,
+					"updateComment", _updateCommentParameterTypes4);
+
+			MethodHandler methodHandler = new MethodHandler(methodKey,
+					commentId, body);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception e) {
+				if (e instanceof com.liferay.portal.kernel.exception.PortalException) {
+					throw (com.liferay.portal.kernel.exception.PortalException)e;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(e);
+			}
+
+			return (com.liferay.portal.kernel.json.JSONObject)returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException se) {
+			_log.error(se, se);
+
+			throw se;
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(ScreensCommentServiceHttp.class);
+	private static final Class<?>[] _addCommentParameterTypes0 = new Class[] {
+			java.lang.String.class, long.class, java.lang.String.class
+		};
+	private static final Class<?>[] _getCommentParameterTypes1 = new Class[] {
+			long.class
+		};
+	private static final Class<?>[] _getCommentsParameterTypes2 = new Class[] {
+			java.lang.String.class, long.class, int.class, int.class
+		};
+	private static final Class<?>[] _getCommentsCountParameterTypes3 = new Class[] {
+			java.lang.String.class, long.class
+		};
+	private static final Class<?>[] _updateCommentParameterTypes4 = new Class[] {
+			long.class, java.lang.String.class
+		};
+}